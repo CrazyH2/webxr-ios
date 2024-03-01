0bde1b228aea154ec23239e8814dd6b78498ca07
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-03-01 08:35:55 +0000

    Create polyfill.js

---
+++ src/polyfill.js
@@ -0,0 +1,11 @@
+ // © Copyright 2022 CrazyH
+ // This file was originally made by CrazyH
+ // Do not rebrand if you are distributing it
+ // © Copyright 2022 CrazyH
+
+export default polyfill {
+  constructor(config) {
+    this.config = config;
+  };
+
+};
