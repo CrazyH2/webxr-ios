8a74c5f0b47de1b41263603c92ef2fb21bd65c56
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-03-01 12:57:28 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,20 @@ export default class WebXR_iOS {
  constructor(config = {}) {
    this.config = Object.freeze(Object.assign({}, config_defaults, config));
   
-   if (!('xr' in window.navigator)) this.run();
+   if (!('xr' in window.navigator) && this.isiOS()) this.run();
+ };
+
+ isiOS() {
+   return [
+     'iPad Simulator',
+     'iPhone Simulator',
+     'iPod Simulator',
+     'iPad',
+     'iPhone',
+     'iPod'
+   ].includes(navigator.platform)
+   // iPad on iOS 13 detection
+   || (navigator.userAgent.includes("Mac") && "ontouchend" in document);
  };
 
  async run() {
