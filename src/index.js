96b2ad7f49f2fde558a05f2be5852b235367cfe4
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-03-01 08:18:47 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+ // © Copyright 2022 CrazyH
+ // This file was originally made by CrazyH
+ // Do not rebrand if you are distributing it
+ // © Copyright 2022 CrazyH
 
+import config_defaults from "./config";
+
+class WebXR_iOS {
+};
+  
+export default WebXR_iOS;
