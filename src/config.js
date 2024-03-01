ba7b806408b3d639ff8100bc70d690b393077806
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-03-01 08:19:22 +0000

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,9 @@
+ // © Copyright 2022 CrazyH
+ // This file was originally made by CrazyH
+ // Do not rebrand if you are distributing it
+ // © Copyright 2022 CrazyH
+
+export default config_defaults = {
+  debug: false,
+  ui: true
+};
